890eae2481e2fd868094adb6f63ca99d4e9f6853
Cico97 <cgiuseppec@hotmail.com>
2022-12-18 19:09:39 +0100

    prova1

--- src/views/ListArtistView.vue
+++ src/views/ListArtistView.vue
@@ -11,14 +11,10 @@
         <br>
         
 
-
-
-
-
     <template>
         <v-card
             class="mx-auto"
-            max-width="500"
+            max-width="600"
         >
 
     <v-container fluid>
@@ -78,8 +74,11 @@ export default {
              { title: 'Francesca Michielin', src: 'https://www.radiobruno.it/wp-content/uploads/2018/07/FRANCESCA-MICHIELIN.jpg', flex: 6 },
              { title: 'Daft Punk', src: 'https://i.discogs.com/sP_wDoC5MsG9lZUfb9thLbpmMmL__nuVnGMNpwgjirE/rs:fit/g:sm/q:90/h:438/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTEyODkt/MTYxNTQ4NDUyOS00/Mjg0LmpwZWc.jpeg', flex: 6 },
              { title: 'Ed Sheeran', src: 'https://st.ilfattoquotidiano.it/wp-content/uploads/2019/07/12/ed-sheeran-905.jpg', flex: 6 },
-             { title: 'Mariya Takeuchi', src: 'https://i.ytimg.com/vi/9Gj47G2e1Jc/sddefault.jpg', flex: 6 },
-             ],
+             { title: 'Lady Gaga', src: 'https://www.umbriajournal.com/wp-content/uploads/2022/10/Lady-Gaga.jpg', flex: 6 },
+             { title: 'Bruno Mars', src: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-911568542-1517221248.jpg', flex: 6 },
+             { title: 'Ariana Grande', src: 'https://media.allure.com/photos/6127ac0b238beb835812ce4c/1:1/w_2250,h_2250,c_limit/Ariana%20Grande%20Allure%20Cover%20No%20Coverlines.jpg', flex: 6 },
+             { title: 'Eminem', src: 'https://www.outpump.com/wp-content/uploads/2022/03/eminem-diamante-outpump.jpeg', flex: 6 },
+            ],
 
             
         }
